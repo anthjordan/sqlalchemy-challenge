dab4c94bafc9fd05390390996082fe293a50c384
anthonyjordan91@gmail.com <anthonyjordan91@gmail.com>
2023-07-15 12:50:07 -0700

    updating work done

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,33 @@
+# Import the dependencies.
+%matplotlib inline
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
